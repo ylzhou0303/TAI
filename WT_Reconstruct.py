34c832056ca630e8b1edd59b38119d946a043ac5
ylzhou0303 <ylzhou0303@gmail.com>
2023-01-08 18:29:01 -0500

    update

--- WT_Reconstruct.py
+++ WT_Reconstruct.py
@@ -13,9 +13,16 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 import os
-os.chdir('C:/Users/yz60069/TAI')
+os.chdir('C:/Users/yz60069/TAI/TAI_fresh')
 import scipy.io
 #mat = scipy.io.loadmat('WaterTableData.mat')
+
+#%% specify the pflotran output structure
+nx = 1
+ny = 1
+nz = 5
+nline = nx * ny * nz
+ntimepoint = 336
 
 
 #%% Specify the variable you want to plot
@@ -30,16 +37,24 @@ elif var_id == 3:
     var_str = 'Total CH4(aq) [M]'
 elif var_id == 4:
     var_str = 'Total DOM1 [M]'
+elif var_id == 5:
+    var_str = 'Total NH4+ [M]'
+elif var_id == 6:
+    var_str = 'microbes [mol/m^3]'
 
-#%% Read in data from PFLOTRAN
-Full_Data = []
-j = 1
+# Read in data from PFLOTRAN
+Full_Data = np.empty( shape = (nline,ntimepoint), dtype = np.float32)
+j = 0
 
-for i in range(0,25):
+
+
+for i in range(0,ntimepoint):
     
-    file_name = 'ferm_test__2-00' + str(i) + '.tec'
-    if len(file_name)>20:
+    file_name = 'TAI_wetlands-00' + str(i) + '.tec'
+    if len(file_name)==21:
         file_name = file_name[0:13] + file_name[14:21]
+    elif len(file_name)==22:
+        file_name = file_name[0:13] + file_name[15:22]
     
     with open(file_name,'r') as inputFile:
         read_lines = inputFile.readlines()
@@ -59,37 +74,61 @@ for i in range(0,25):
     for i in range(0, len(parameter_list)):
         results[parameter_list[i]] = data_list[:,i]
     
-    Full_Data.append(results[var_str])
+    Full_Data[:,j] = results[var_str]
+    j = j + 1
 
 
-Full_Data = np.array(Full_Data)
-
-#%% Plot the depth profile of variable investigated
-
-ydata = results['Z [m]'] - 1  #minus the depth of the soil profile
-for i in range(0, 2):
-    xdata = Full_Data[i,:]
-    plt.plot(xdata, ydata)
 
 
 
 #%%
+Coord = np.empty( shape = (nline,3) , dtype = np.float32 )
+Coord[:,0] = results['VARIABLES=X [m]']
+Coord[:,1] = results['Y [m]']
+Coord[:,2] = results['Z [m]']
+
+
+
+#%% Plot the depth profiles of the investigated variable for different timepoints
+interval = nx * ny
+depths = Coord[0:nline:interval,2] - 1  #minus the depth of the soil profile
+
+for i in range(0,ntimepoint):
+    conc = Full_Data[0:nline:interval,i] 
+    plt.plot(conc, depths)
+
+ax=plt.gca()
+ax.set_xlabel(var_str)
+ax.set_ylabel('Soil Depth (m)')
+plt.rcParams.update({'font.size': 12})
+
+#%% plot the time series of the variable
+
+plt.plot(Full_Data[4,:])   #bigger row number means closer to the soil surface
+ax=plt.gca()
+ax.set_xlabel('Time (hr)')
+ax.set_ylabel(var_str)
+plt.rcParams.update({'font.size': 13})
+
+ #%% plot O2 and liquid saturation profile
 fig, ax1 = plt.subplots()
 ax2 = ax1.twinx()
 
-ax1.plot(Full_Data_ls[1:,1], 'b-')   #larger column number, closer to the soil surface
+ax1.plot(Full_Data_ls[:,4], 'b-')   #larger column number, closer to the soil surface
 ax1.spines['left'].set_color('b')
+ax1.set_ylabel('Liquid Saturation')
 
-
-ax2.plot(Full_Data_o2[0:,1], 'r-')
+ax2.plot(Full_Data_o2[:,4], 'r-')
 ax2.spines['right'].set_color('r')
+ax2.set_ylabel('O2(aq) [M]')
 
-
-#%% 
+#%% import the observation data of water table depth 
+os.chdir('C:/MBL/Research/Typha data/water table')
 WTMean = []
 
+
 from csv import reader
-file_name = "WTMean.csv"
+file_name = "WTMeanCB.csv"
 
 with open (file_name, 'r') as csv_file:
     csv_data = reader(csv_file)
@@ -100,35 +139,75 @@ with open (file_name, 'r') as csv_file:
 
 WTMean = np.array(WTMean)
 
-#%% compare the modeled water level with the observation
+#%%  Plot the observed water table with the modeled liquid saturation
 fig, ax1 = plt.subplots()
-ax2 = ax1.twinx()
 
-ax1.plot(Full_Data[1:,1], 'r-o')
-ax1.spines['left'].set_color('r')
+ax1.plot(Full_Data[4,:], 'r-', label = 'Liquid Saturation (PFLOTRAN)')  
 ax1.set_ylim(0,1.1)
-ydata = np.array(WTMean[200:248,1], dtype = float)
-ax2.plot(range(0,48), ydata, 'b-o')
-ax2.plot(range(0,50), [3.11]*50, 'b-')
+ax1.set_ylabel('Liquid Saturation', color = 'r')
+
+ax2 = ax1.twinx()
+ydata = np.array(WTMean[1515:1850, 1], dtype = float)
+ax2.plot(range(0,335), ydata, 'b-', label = 'Water Table')
+ax2.plot([0, 335], [0.95, 0.95], 'k-', label = 'Soil Surface')
 ax2.spines['right'].set_color('b')
-ax2.set_ylim(2.95, 3.5)
+ax2.spines['left'].set_color('r')
+ax2.set_ylabel('Water Table(m)', color = 'b')
+#ax2.set_ylim(2.95, 3.5)
 
-
+ax1.set_xlabel('Time (hr)')
+plt.rcParams.update({'font.size': 13})
+fig.legend(loc = 'lower left', bbox_to_anchor=(0,0.05), bbox_transform=ax.transAxes, fontsize = 10)
 
 #%% Convert the liquid saturation to water table level
 WT_mod = []
-D = [0.5, 0.5]
+WaterHeight = []
+D = [0.2]*5
 Area = 1 * 1
+soil_depth = 1
 Vol = np.array(D) * Area
 
 
-for arr in Full_Data:
-    WT_mod.append(sum(arr * Vol) / Area - sum(D) + 3.1149 )
+for i in range():
+    WaterHeight.append(sum(arr * Vol) /Area)
+    WT_mod.append(sum(arr * Vol) / Area + (0.95 - soil_depth) )
 
 
 
 
 
-#%%
-plt.plot(range(1,49), np.array(WTMean[200:248,1], dtype = np.float32))
+#%% compare the modeled water level with the observation
+plt.plot(range(1,25), np.array(WTMean[200:248:2,1], dtype = np.float32))
 plt.plot(WT_mod, 'r.')
+
+
+
+#%% Compare modeled O2 and observed O2
+#%% import O2 data
+os.chdir('C:/MBL/Research/Typha data/Redox')
+OxyConc = []
+
+
+from csv import reader
+file_name = "Oxygen_Conc_CB.csv"
+
+with open (file_name, 'r') as csv_file:
+    csv_data = reader(csv_file)
+    header = next(csv_file)
+    
+    for row in csv_data:
+        OxyConc.append(row)
+
+OxyConc = np.array(OxyConc)
+
+#%% Plot the modeled O2 against observed O2
+plt.plot(Full_Data[1,:]*32*1000, 'b--', label = 'Modeled O2')
+
+ydata = np.array(OxyConc[3773:7805:12,24], dtype = float)
+plt.plot(ydata, 'b-', label = 'Estimated O2 (redox)')
+plt.legend(loc = 0)
+
+ax = plt.gca()
+ax.set_xlabel('Time (hr)')
+ax.set_ylabel('O2(aq) (mg/L)')
+ax.set_ylim(-0.1, 4.2)
